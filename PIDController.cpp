76133a705b497c515acc1926445a14d093f97cbb
Trevor Von Seggern <tvoman2@gmail.com>
2016-04-25 14:26:34 -0600

    updated

--- PIDController.cpp
+++ PIDController.cpp
@@ -9,10 +9,10 @@ void PIDController::Update() {
 	if (CurrentTime >= LastInputTime + Frequency) {
 		Input.UpdateValue();
 		Error = Input.InputValue - *Target;
-		double timeSlice = CurrentTime - LastInputTime;
+		float timeSlice = CurrentTime - LastInputTime;
 		P = Pp * Error;
 		I += Ip * Error / timeSlice;
-		D = (float) (Dp * (Error - PreviousError) / timeSlice);
+		D = Dp * (Error - PreviousError) / timeSlice;
 		OutputValue = P + I + D;
 		PreviousError = Error;
 	}
